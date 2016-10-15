945bc64bf599f1568f8a6908bae9cd6e187dd039
Warodom Khamphanchai <kwarodom@vt.edu>
2016-10-14 23:40:42 -0700

    fix bugs for complete project

--- winerama/reviews/suggestions.py
+++ winerama/reviews/suggestions.py
@@ -5,11 +5,13 @@ from scipy.sparse import dok_matrix, csr_matrix
 import numpy as np
 
 def update_clusters():
+    print("inside update clusters")
     num_reviews = Review.objects.count()
     update_step = ((num_reviews/100)+1) * 5
-    if num_reviews % update_step == 0: # using some magic numbers here, sorry...
+    print("{}: ".format(num_reviews % update_step))
+    if num_reviews % update_step != 0: # using some magic numbers here, sorry...
         # Create a sparse matrix from user reviews
-        all_user_names = map(lambda x: x.username, User.objects.only("username"))
+        all_user_names = list(map(lambda x: x.username, User.objects.only("username")))
         all_wine_ids = set(map(lambda x: x.wine.id, Review.objects.only("wine")))
         num_users = len(all_user_names)
         ratings_m = dok_matrix((num_users, max(all_wine_ids)+1), dtype=np.float32)
@@ -30,3 +32,6 @@ def update_clusters():
             cluster.save()
         for i,cluster_label in enumerate(clustering.labels_):
             new_clusters[cluster_label].users.add(User.objects.get(username=all_user_names[i]))
+        print("Cluster updated!!!!!!!!!!!!!!!!!!!!!!!!")
+        print("Cluster updated!!!!!!!!!!!!!!!!!!!!!!!!")
+        print("Cluster updated!!!!!!!!!!!!!!!!!!!!!!!!")
